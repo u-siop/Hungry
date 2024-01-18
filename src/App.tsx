dd108fcedd57288e51b3c0f5f43dd2118ca8c036
u-siop <jinwook.jun5@gmail.com>
2024-01-18 09:54:30 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="centered">
+        <button>Click Me</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
